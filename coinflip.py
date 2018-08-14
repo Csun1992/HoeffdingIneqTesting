dcc7fd5ee8a2b822869c7fc2d0b9be3fc699ee5a
Csun1992 <sonshicyu@gmail.com>
2018-08-14 02:05:44 -0500

    Set up the experiment

--- coinflip.py
+++ coinflip.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 class Coin:
-    def __init__(self):
-        self.flipTimes = 0
+    def __init__(self, flipTimes):
+        self.flipTimes = flipTimes 
         self.results = []
     
     def updateResults(self, randNumber):
@@ -11,8 +11,9 @@ class Coin:
         else: self.results.append(1)
 
     def flip(self):
-        self.flipTimes += 1
-        self.updateResults(np.random.rand(1))
+        if self.flipTimes == 0: return 
+        for i in range(self.flipTimes):
+            self.updateResults(np.random.rand(1))
 
     def getTotalHeads(self):
         return sum(self.results) 
@@ -22,3 +23,26 @@ if __name__ == "__main__":
     coinNum = 1000
     flipTime = 10
     experimentTimes = 100000
+    coins = []
+    headCounts = []
+    firstCoinHeads = 0
+    minCoinHeads = 0
+    randCoinHeads = 0
+    for i in range(experimentTimes):
+        for j in range(coinNum):
+            coin = Coin(flipTime) 
+            coins.append(coin)
+            headCounts.append(coin.getTotalHeads())
+
+        firstCoinHeads += headCounts[0]/float(flipTime*experimentTimes)
+        randCoinHeads += headCounts[np.random.randint(coinNum)]/float(flipTime*experimentTimes)
+        minCoinHeads += min(headCounts)/float(flipTime*experimentTimes) 
+
+    print "first coin heads"
+    print firstCoinHeads
+
+    print "random coin heads"
+    print randCoinHeads
+
+    print "min coin heads"
+    print minCoinHeads
